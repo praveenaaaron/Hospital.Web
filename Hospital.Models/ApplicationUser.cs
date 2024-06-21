0fc152bff118d71f216234eef937136ada84c40b
praveenaaaron <piravienamca12@gmail.com>
2024-06-21 18:06:03 +0530

    Hospital models completed

--- Hospital.Models/ApplicationUser.cs
+++ Hospital.Models/ApplicationUser.cs
@@ -9,5 +9,22 @@ namespace Hospital.Models
 {
     public class ApplicationUser :IdentityUser
     {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Nationality { get; set; }
+        public string Address { get; set; }
+        public DateTime DOB { get; set; }   
+
+        public string Specialist { get; set; }
+        public Department Department { get; set; }
+        public ICollection<Appointment> Appointments { get; set; }   
+        public ICollection <Payroll> Payrolls { get; set; }  
+    }
+}
+namespace Hospital.Models
+{
+    public enum Gender
+    {
+        Male,Female,Others
     }
 }
