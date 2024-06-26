3548f9161b936fb8f577be0ab7e6e1b27bd0d526
praveenaaaron <piravienamca12@gmail.com>
2024-06-26 10:45:06 +0530

    update model to database completed

--- Hospital.Models/ApplicationUser.cs
+++ Hospital.Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +18,9 @@ namespace Hospital.Models
 
         public string Specialist { get; set; }
         public Department Department { get; set; }
-        public ICollection<Appointment> Appointments { get; set; }   
+        [NotMapped]
+        public ICollection<Appointment> Appointments { get; set; }
+        [NotMapped]
         public ICollection <Payroll> Payrolls { get; set; }  
     }
 }
